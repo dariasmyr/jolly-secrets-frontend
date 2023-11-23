640f4329707952e0f6ed44c660d955c3f3702a4c
daria_smyr <dasha.smyr@gmail.com>
2023-11-24 00:08:47 +0300

    Small fix

--- src/components/ui/custom/event-page/styled-components.tsx
+++ src/components/ui/custom/event-page/styled-components.tsx
@@ -8,7 +8,7 @@ export const Wrapper = styled.div`
 export const ImageWrapper = styled(Image)`
   width: 368px;
   height: 200px;
-  border-radius: 30px 30px 0 0;
+  border-radius: 20px 20px 0 0;
 `;
 
 export const Content = styled.div`
