c50cbb1251d2e4be9042ebc3f6bd6b1b51d95dd6
Daria Smyr <68226469+dariasmyr@users.noreply.github.com>
2023-11-24 09:26:12 +0000

    Create group.tsx

---
+++ public/locales/en/group.tsx
@@ -0,0 +1,56 @@
+{
+  "group": {
+    "change": {
+      "title": "Change"
+    },
+    "delete": {
+      "title": "Delete",
+      "dialog": {
+        "title": "Are you sure?",
+        "description": "Are you sure you want to delete this group? This action cannot be undone.",
+        "cancel": "Cancel ❌",
+        "confirm": "Yes, delete group ✔️"
+      },
+      "success": "Group successfully deleted"
+    }
+  },
+  "groups": {
+    "private": "My groups",
+    "public": "Public groups",
+    "no_groups_title": "No groups yet",
+    "no_groups_description": "Create your first group!",
+    "one_member": "person",
+    "two_members": "people",
+    "many_members": "people",
+    "one_event": "active event",
+    "two_events": "active events",
+    "many_events": "active events"
+  },
+  "create_group": {
+    "duplicate_name": "Group name already exists",
+    "header": "Create group",
+    "choose_cover": "Choose cover",
+    "group_name": "Event name",
+    "group_description": "Event description",
+    "access_level": {
+      "title": "Access level",
+      "public": "Public",
+      "private": "Private"
+    },
+    "invite": {
+      "title": "Participants",
+      "description": "Click the button to generate an invitation",
+      "action": "Invite",
+      "dialog": {
+        "title": "Copy and send to a friend",
+        "label": "Link - invitation",
+        "cancel": "Close ❌",
+        "success": "Link copied",
+        "generate": "Invite more 📝"
+      },
+      "success": "Group successfully created"
+    },
+    "action": "Create group",
+    "back": "Back to group list"
+  }
+}
