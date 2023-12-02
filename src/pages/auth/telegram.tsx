69e4e8ea99435942e875ca45480daf2441ab3061
daria_smyr <dasha.smyr@gmail.com>
2023-12-03 00:19:34 +0300

    Added wait for token to be set

--- src/pages/auth/telegram.tsx
+++ src/pages/auth/telegram.tsx
@@ -33,6 +33,9 @@ const Telegram: FC = () => {
       authStore.setToken(loginData.loginWithTelegram.token);
       authStore.setAccount(loginData.loginWithTelegram.account);
 
+      // wait for token to be set
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+
       await router.push('/public-groups');
     })();
   }, [token]);
