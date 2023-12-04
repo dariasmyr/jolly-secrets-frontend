fdc41bb7de4a35d5b11ef18e4f07262f0a8c7a55
daria_smyr <dasha.smyr@gmail.com>
2023-12-04 23:01:11 +0300

    Meta viewport

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -47,6 +47,10 @@ export default class AppDocument extends Document {
 
     return (
       <Html lang={currentLocale}>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+        />
         <Head />
         <body>
           <Main />
