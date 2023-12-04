e33267e5ba4a0f33fa1819bb6e30ebb68a9e6a58
daria_smyr <dasha.smyr@gmail.com>
2023-12-04 13:24:53 +0300

    Moved public-groups to index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,201 +1,248 @@
-import { ReactNode } from 'react';
-import CollapsedBreadcrumbs from '@components/ui/common/breadcrumbs';
+import { FC, useEffect, useState } from 'react';
+import { GetServerSideProps } from 'next';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
+import { useTranslation } from 'next-i18next';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { FabAdd } from '@components/ui/common/fab-add';
 import { Page } from '@components/ui/common/page';
-import { Stepper } from '@components/ui/common/stepper';
-import { DialogInputEmail } from '@components/ui/custom/dialog-input-email';
-import { Button, ButtonVariant } from 'src/components/ui/common/button';
-import { FabAdd } from 'src/components/ui/common/fab-add';
-import { MenuOptions } from 'src/components/ui/common/menu-options';
-import { ButtonLarge } from 'src/components/ui/custom/button-large';
-import { CardChangeName } from 'src/components/ui/custom/card-change-name';
-import { CardDeleteAccount } from 'src/components/ui/custom/card-delete-account';
-import { CardGenerateInvite } from 'src/components/ui/custom/card-generate-invite';
-import {
-  CardImage,
-  ICardImageProperties,
-} from 'src/components/ui/custom/card-image';
-import { DialogChooseAccount } from 'src/components/ui/custom/dialog-choose-account';
+import { CardImage } from '@components/ui/custom/card-image';
+import Alert from '@mui/material/Alert';
+import Snackbar from '@mui/material/Snackbar';
 import { DialogConfirmAction } from 'src/components/ui/custom/dialog-confirm-action';
+import styled from 'styled-components';
 
-const testCardImageProperties: ICardImageProperties = {
-  imageUrl: 'https://source.unsplash.com/random/368×200/?fruit',
-  preHeader: 'Pre Header Text',
-  header: 'Header Text',
-  text: 'Body Text',
-  tags: [
-    {
-      title: 'Test Tag 1',
-      warning: true,
-    },
-    {
-      title: 'Test Tag 2',
-    },
-    {
-      title: 'Test Tag 3',
-    },
-  ],
-  menu: {
-    options: [
-      {
-        title: 'Изменить',
-        onClick: (): void => {
-          // eslint-disable-next-line no-alert
-          alert('Изменить');
-        },
-      },
-      {
-        title: 'Удалить',
-        onClick: (): void => {
-          // eslint-disable-next-line no-alert
-          alert('Удалить');
-        },
-      },
-    ],
-  },
-};
+import { Header } from '@/components/ui/common/page/styled-components';
+import {
+  StyledImage,
+  SubText,
+  Text,
+} from '@/components/ui/common/styled-components';
+import {
+  EventStatus,
+  GroupMemberRole,
+  GroupStatus,
+  useDeleteGroupMutation,
+  useGetAccountCountQuery,
+  usePublicGroupsQuery,
+} from '@/generated/graphql';
+import { useAuthStore } from '@/store/auth.store';
 
-const steps = [
-  {
-    label: 'Подготовка',
-    description: 'Первый шаг в процессе',
-    showDescription: true,
-    completed: true,
-  },
-  {
-    label: 'Исполнение',
-    description: 'Второй шаг в процессе',
-    showDescription: false,
-    completed: false,
-  },
-  {
-    label: 'Завершение',
-    description: 'Третий и заключительный шаг в процессе',
-    showDescription: true,
-    completed: false,
-  },
-];
+function pluralize(
+  number: number,
+  one: string,
+  two: string,
+  many: string,
+): string {
+  const lastDigit = number % 10;
+  const lastTwoDigits = number % 100;
 
-export default function IndexPage(): ReactNode {
-  const handleClick = (): void => {
+  if (lastDigit === 1 && lastTwoDigits !== 11) {
+    return one;
+  }
+
+  if ([2, 3, 4].includes(lastDigit) && ![12, 13, 14].includes(lastTwoDigits)) {
+    return two;
+  }
+
+  return many;
+}
+
+const Index: FC = () => {
+  const { t } = useTranslation(['common', 'group', 'event', 'menu']);
+  const authStore = useAuthStore();
+  const router = useRouter();
+  const [isDialogOpen, setDialogOpen] = useState(false);
+  // eslint-disable-next-line unicorn/no-null
+  const [groupToDelete, setGroupToDelete] = useState<number | null>(null);
+  const [deleteGroup] = useDeleteGroupMutation();
+
+  const [snackbarData, setSnackbarData] = useState({
+    open: false,
+    message: '',
+  });
+
+  useEffect(() => {
+    if (!authStore.token || !authStore.account?.id) {
+      router.push('/auth/login');
+    }
+  }, [authStore]);
+
+  const { data, error, loading, refetch } = usePublicGroupsQuery({
+    variables: {
+      offset: 0,
+      limit: 100,
+    },
+  });
+
+  const {
+    data: accountCountData,
+    error: accountCountError,
+    loading: accountCountLoading,
+  } = useGetAccountCountQuery({});
+
+  const createGroup = (): void => {
     // eslint-disable-next-line no-alert
-    alert('click');
+    router.push('/create-group');
   };
+
+  if (loading || accountCountLoading) {
+    // eslint-disable-next-line sonarjs/no-duplicate-string
+    return <Page title={'Jolly Secrets'}>Loading...</Page>;
+  }
+
+  if (error || accountCountError) {
+    return <Page title={'Jolly Secrets'}>Error: {JSON.stringify(error)}</Page>;
+  }
   return (
-    <Page
-      title={'Home page'}
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        paddingLeft: 16,
-        paddingRight: 16,
-        gap: '20px',
-        marginTop: '100px',
-        marginBottom: '100px',
-      }}
-    >
-      <CollapsedBreadcrumbs
-        steps={[
-          { label: 'Публичные группы', link: '#', onClick: handleClick },
-          { label: 'Тайные Санты Грушевки', link: '#', onClick: handleClick },
-          {
-            label: 'Событие: тайные санты грушевки',
-            link: '#',
-            onClick: handleClick,
-          },
-        ]}
-      />
-      <Button onClick={handleClick} variant={ButtonVariant.primary}>
-        я получил подарок
-      </Button>
-      <Button onClick={handleClick} variant={ButtonVariant.secondary}>
-        я не получил подарок
-      </Button>
-      <Button onClick={handleClick} variant={ButtonVariant.borderless}>
-        Отмена
-      </Button>
-      <Button onClick={handleClick} variant={ButtonVariant.warning}>
-        Удалить
-      </Button>
-      <Button onClick={handleClick} variant={ButtonVariant.outlined}>
-        Удалить
-      </Button>
-      <CardImage
-        imageUrl={testCardImageProperties.imageUrl}
-        text={testCardImageProperties.text}
-        header={testCardImageProperties.header}
-        preHeader={testCardImageProperties.preHeader}
-        tags={testCardImageProperties.tags}
-        menu={testCardImageProperties.menu}
-      />
-      <CardGenerateInvite
-        title={'Добавить участников'}
-        description={'Нажми на кнопку чтобы сгенерировать приглашение.'}
-        onGenerateInviteClick={handleClick}
-        button={'Добавить участников'}
-      />
-      <CardChangeName
-        label="Имя пользователя"
-        placeholder="Введите новое имя"
-        onSaveClick={handleClick}
-        button="Сохранить"
-      />
-      <CardDeleteAccount
-        description={'Чтобы удалить аккаунт напишите “Удалить аккаунт”'}
-        placeholder={'Удалить'}
-        onDeleteButtonClick={handleClick}
-        button={'Удалить'}
-      />
-      <ButtonLarge onClick={handleClick}>Хочу учавстовать!</ButtonLarge>
-      <ButtonLarge disabled>Завершено 01.12.2023</ButtonLarge>
-      <MenuOptions
-        options={[
-          {
-            title: 'Изменить',
-            onClick: (): void => {
-              // eslint-disable-next-line no-alert
-              alert('Изменить');
-            },
-          },
-          {
-            title: 'Удалить',
-            onClick: (): void => {
-              // eslint-disable-next-line no-alert
-              alert('Удалить');
-            },
-          },
-        ]}
-      />
+    <Page title={'Jolly Secrets'} style={{ gap: 16, marginTop: 24 }}>
+      <Header>{t('group:groups.public')}</Header>
+      {data?.publicGroups.length === 0 && (
+        <Wrapper>
+          <StyledImage>
+            <Image
+              src={'/assets/sand-clock.png'}
+              width={100}
+              height={100}
+              alt="Wait"
+            />
+          </StyledImage>
+          <Text>{t('group:groups.no_groups_title')}</Text>
+          <SubText>{t('group:groups.no_groups_description')}</SubText>
+        </Wrapper>
+      )}
+      {data?.publicGroups.map((group) => {
+        const isAdmin = group.members!.some(
+          (member) =>
+            member.role === GroupMemberRole.Admin &&
+            member.accountId === authStore.account?.id,
+        );
+
+        let tags = [];
+
+        tags = isAdmin
+          ? [
+              {
+                title: t('group:groups.creator'),
+                warning: true,
+              },
+            ]
+          : [
+              {
+                title: `${accountCountData?.getAccountCount} ${pluralize(
+                  accountCountData!.getAccountCount!,
+                  t('group:groups.one_member'),
+                  t('group:groups.two_members'),
+                  t('group:groups.many_members'),
+                )}`,
+              },
+            ];
+        return (
+          <CardImage
+            key={group.id}
+            imageUrl={
+              group.pictureUrl
+                ? process.env.NEXT_PUBLIC_REST_API_URL + group.pictureUrl
+                : '/assets/hover.jpg'
+            }
+            preHeader={`${group.events?.filter(
+              (event) => event.status === EventStatus.Open,
+            )?.length} ${pluralize(
+              group.events!.filter((event) => event.status === EventStatus.Open)
+                ?.length,
+              t('event:events.one_еvent'),
+              t('event:events.two_еvents'),
+              t('event:events.many_еvents'),
+            )}`}
+            header={group.name}
+            text={group.description}
+            tags={tags}
+            menu={
+              isAdmin
+                ? {
+                    options: [
+                      {
+                        title: t('group:group.change.title'),
+                        onClick: (): void => {
+                          router.push(`/update-group?id=${group.id}`);
+                        },
+                      },
+                      {
+                        title: t('group:group.delete.title'),
+                        onClick: (): void => {
+                          setGroupToDelete(group.id);
+                          setDialogOpen(true);
+                        },
+                      },
+                    ],
+                  }
+                : undefined
+            }
+            onClick={(): void => {
+              router.push(`/events?groupId=${group.id}`);
+            }}
+          />
+        );
+      })}
+      <FabAdd onClick={createGroup} />
       <DialogConfirmAction
-        isOpen={false}
-        title="Вы уверены?"
-        description="После удаления аккаунта нельзя будет восстановить."
-        cancelButtonText="Отмена"
-        confirmButtonText="Да, удалить аккаунт"
-        onCancelClick={handleClick}
-        onConfirmClick={handleClick}
+        isOpen={isDialogOpen}
+        title={t('group:group.delete.dialog.title')}
+        description={t('group:group.delete.dialog.description')}
+        cancelButtonText={t('group:group.delete.dialog.cancel')}
+        confirmButtonText={t('group:group.delete.dialog.confirm')}
+        onCancelClick={(): void => setDialogOpen(false)}
+        onConfirmClick={async (): Promise<void> => {
+          if (!groupToDelete) {
+            return;
+          }
+          const { data: deletedGroup } = await deleteGroup({
+            variables: { id: groupToDelete },
+          });
+          if (deletedGroup?.deleteGroup.status === GroupStatus.Closed) {
+            // eslint-disable-next-line unicorn/no-null
+            setGroupToDelete(null);
+            await refetch();
+            setDialogOpen(false);
+            setSnackbarData({
+              open: true,
+              message: t('group:group.delete.success'),
+            });
+          }
+        }}
       />
-      <DialogInputEmail
-        initialEmail={''}
-        isOpen={false}
-        title="Укажите почту"
-        onCancelClick={handleClick}
-        onSaveClick={handleClick}
-      />
-      <DialogChooseAccount
-        isOpen={false}
-        title="Выберите аккаунт, который хотите оставить"
-        description="У вас уже есть аккаунт, привязанный к Telegram/Google профилю. Выберите аккаунт, который хотите оставить, и мы привяжем к нему ваш Telegram/Google профиль."
-        telegramButtonText="Аккаунт Telegram"
-        googleButtonText="Аккаунт Google"
-        cancelButtonText="Отмена"
-        onTelegramClick={handleClick}
-        onGoogleClick={handleClick}
-        onCancelClick={handleClick}
-      />
-      <FabAdd onClick={handleClick} />
-      <Stepper steps={steps} />
+      <Snackbar
+        open={snackbarData.open}
+        autoHideDuration={3000}
+        onClose={(): void => setSnackbarData({ ...snackbarData, open: false })}
+      >
+        <Alert severity="warning" sx={{ width: '100%' }}>
+          {snackbarData.message}
+        </Alert>
+      </Snackbar>
     </Page>
   );
-}
+};
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 50vh;
+`;
+
+export const getServerSideProps: GetServerSideProps = async ({ locale }) => {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale ?? 'en', [
+        'group',
+        'common',
+        'event',
+        'menu',
+      ])),
+    },
+  };
+};
+
+export default Index;
